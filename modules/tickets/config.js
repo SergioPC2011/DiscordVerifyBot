c8ee375705dd451bd1c2a29c3304e7426965ff68
SergioPC2011 <sergiojuarezcano11@gmail.com>
2026-08-01 19:15:32 +0200

    Mover módulos de tickets a la carpeta modules

---
+++ modules/tickets/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+    // Servidor
+    guildId: "1515037603219509309",
+
+    // Canal donde se enviará el panel de tickets
+    panelChannel: "1515038352972451950",
+
+    // Rol que puede ver y gestionar todos los tickets
+    supportRole: "1515038213998379018",
+
+    // Color del embed
+    embedColor: "#FFD400",
+
+    // Nombre del sistema
+    botName: "YELLOW DEVILS"
+
+};
