afc2b56e80444ec101add81e90539c10ae38ee33
SergioPC2011 <sergiojuarezcano11@gmail.com>
2026-09-20 16:53:11 +0200

    Fix ticket categories config

--- modules/tickets/config.js
+++ modules/tickets/config.js
@@ -1,18 +1,25 @@
-module.exports = {
 
-    // Servidor
+module.exports = {
     guildId: "1515037603219509309",
 
-    // Canal donde se enviará el panel de tickets
     panelChannel: "1515038352972451950",
 
-    // Rol que puede ver y gestionar todos los tickets
-    supportRole: "1515038213998379018",
+    botName: "Yellow Shop | Ticket System",
 
-    // Color del embed
     embedColor: "#FFD400",
 
-    // Nombre del sistema
-    botName: "YELLOW DEVILS"
+    supportRole: "1515038213998379018",
 
+    supervisorRole: "1551216884169445476",
+
+    ticketCategories: {
+        remplace: "1551217135500394496",
+        partner: "1551217199228915733",
+        buy: "1551217314454700072",
+        support: "1551217460450037780"
+    },
+
+    claimedCategory: "1551223584825811035",
+
+    claimedCategoryName: "🔒 TICKETS GOTIKURT"
 };
